7df9753cf409139227272723ed03279d78e3919c
mariKaren <karenbmari@gmail.com>
2025-02-14 21:14:33 +0100

    Maquetacion y estilos de la seccion Novedades

--- scripts/home.js
+++ scripts/home.js
@@ -49,5 +49,49 @@ function createRecommendedsItems(array){
 sectionRecommendeds.appendChild(recommendedsTitle)
 createRecommendedsItems(recommendeds)
 
+/* FIN SECCION RECOMENDADOS */
+
 /* SECCION NOVEDADES */
 const sectionNews=document.querySelector(".section__news");
+const divHeadingNews= document.createElement("div");
+    divHeadingNews.classList.add("news__heading");
+    divHeadingNews.innerHTML=`
+        <h3 class="subtitle c_Brown news__heading-title">Novedades</h3>
+        <a href="" class="news__heading-more c_Orange">Ver más</a>`
+sectionNews.appendChild(divHeadingNews)
+
+const newsTitles=[
+    {
+        nombre:"Nuevos títulos que están arrasando este Febrero 2025",
+        img:"./images/novedades.jpg"
+    },
+    {
+        nombre:"Títulos confirmados para Marzo 2025",
+        img:"./images/confirmados.jpg"
+    }
+]
+
+/*-------------------------------INCORPORAR LINK A PAGINA NOVEDADES  */
+function createNewsSection(array){
+    const div=document.createElement("div");
+    div.classList.add("news__container-titles");
+    array.forEach(element => {
+        div.innerHTML+=`
+        <div class="news__options">
+                <div class="news__image">
+                    <div class="news__overlay">
+                        <a href=""><h3 class="news__title ">${element.nombre}</h3> </a>
+                    </div>
+                </div>
+        </div>`
+    });
+
+    const divImages=div.querySelectorAll(".news__image");
+    divImages.forEach((image,index)=>{
+        image.style.backgroundImage=`url('${newsTitles[index].img}')`;
+    })
+    sectionNews.appendChild(div);
+    
+}
+
+createNewsSection(newsTitles)
