a08ac7ecc1bb8904d9a63fe95ea2a4c92865bee8
mariKaren <karenbmari@gmail.com>
2025-02-16 15:12:01 +0100

    Maquetacion inicial informacion libro

--- scripts/libros.js
+++ scripts/libros.js
@@ -1,11 +1,11 @@
 /* Seleccion en html */
-const sectionSearch=document.querySelector(".section__search");
-const sectionSearchResults=document.querySelector(".section__search-results");
-const sectionTips=document.querySelector(".section__tips");
+const sectionSearch = document.querySelector(".section__search");
+const sectionSearchResults = document.querySelector(".section__search-results");
+const sectionTips = document.querySelector(".section__tips");
 
 /*Seccion de BUSQUEDA*/
-function createSearch(){
-    sectionSearch.innerHTML=`
+function createSearch() {
+    sectionSearch.innerHTML = `
                 <h1 class="section__search-title subtitle c_Brown">Busca un libro</h1>
                 <div class="search__container">
                     <div class="search__container-inputs">
@@ -14,101 +14,131 @@ function createSearch(){
                     </div>
                     <button class="c_Beige search_button">Buscar</button>
                 </div>`
-    const searchBtn=document.querySelector(".search_button");
-   
-    searchBtn.addEventListener("click",()=>{
+    const searchBtn = document.querySelector(".search_button");
+
+    searchBtn.addEventListener("click", () => {
         sectionTips.classList.add("d-none");
         loadingData(sectionSearchResults)
-        /* sectionSearchResults.innerHTML=`BUSCANDO` */
-        const searchByName=document.querySelector(".search__container-input_name").value;
-        const searchByAuthor=document.querySelector(".search__container-input_author").value;
-        searchBook(searchByName,searchByAuthor);
+        const searchByName = document.querySelector(".search__container-input_name").value;
+        const searchByAuthor = document.querySelector(".search__container-input_author").value;
+        searchBook(searchByName, searchByAuthor);
     })
 }
 
-function searchBook(name,author){
+function searchBook(name, author) {
     let url = "https://openlibrary.org/search.json?";
 
-    if(name || author){
+    if (name || author) {
         if (name && author) {
-                url += `title=${encodeURIComponent(name)}&author=${encodeURIComponent(author)}`;
+            url += `title=${encodeURIComponent(name)}&author=${encodeURIComponent(author)}`;
         } else if (name) {
-                url += `title=${encodeURIComponent(name)}`;
-        } else{
-                url += `author=${encodeURIComponent(author)}`;
+            url += `title=${encodeURIComponent(name)}`;
+        } else {
+            url += `author=${encodeURIComponent(author)}`;
         }
         url += `&limit=50&offset=0`;
-     }
-     else {
-        let randomNumber = Math.floor(Math.random() * 971); 
+    }
+    else {
+        let randomNumber = Math.floor(Math.random() * 971);
         url += `q=book&limit=20&offset=${randomNumber}`;
     }
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    sectionSearchResults.classList.remove("loading_data")
-                    sectionSearchResults.innerHTML=" "
-                    data.docs.forEach(libro=>{createResultsCarts(libro,sectionSearchResults)});
-                })
-                .catch(error => console.error('Error al obtener los datos:', error));
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            sectionSearchResults.classList.remove("loading_data")
+            sectionSearchResults.innerHTML = " "
+            data.docs.forEach(libro => { createResultsCarts(libro, sectionSearchResults) });
+        })
+        .catch(error => console.error('Error al obtener los datos:', error));
 }
 
-function createResultsCarts(libro,seccion){
-        let cantAutor= libro.author_name ? (libro.author_name.length > 1 ? "Autores" : "Autor") : "Autor desconocido";
-        let autor;
-        if (libro.author_name && libro.author_name.length > 3) {
-            autor = libro.author_name.slice(0, 3).join(", ") + "..."; 
-        } else {
-            autor = libro.author_name ? libro.author_name.join(", ") : "Autor desconocido";
-        }
-        let imgLibro;
-        if(libro.cover_i==undefined){
-            imgLibro="../images/novedades.jpg"
-        }else{
-            imgLibro=`https://covers.openlibrary.org/b/id/${libro.cover_i}-M.jpg`
-        }
-        const article=document.createElement("article");
-        article.classList.add("search__result-book")
-        article.innerHTML = `
+function createResultsCarts(libro, seccion) {
+    let cantAutor = libro.author_name ? (libro.author_name.length > 1 ? "Autores" : "Autor") : "Autor desconocido";
+    let autor;
+    if (libro.author_name && libro.author_name.length > 3) {
+        autor = libro.author_name.slice(0, 3).join(", ") + "...";
+    } else {
+        autor = libro.author_name ? libro.author_name.join(", ") : "Autor desconocido";
+    }
+    let imgLibro;
+    if (libro.cover_i == undefined) {
+        imgLibro = "../images/novedades.jpg"
+    } else {
+        imgLibro = `https://covers.openlibrary.org/b/id/${libro.cover_i}-M.jpg`
+    }
+    const article = document.createElement("article");
+    article.classList.add("search__result-book")
+    article.innerHTML = `
                     <div class=search__result-book_inf>
                     <h3 class="result-book_inf-title c_Brown">${libro.title}</h3>
                     <p><strong>${cantAutor}:</strong> ${autor}</p>
                     <p><strong>Año de Publicación:</strong> ${libro.first_publish_year || 'N/A'}</p>
+                    <button class="book_inf-${libro.cover_i} book_inf-btn" >Ver libro</button>
                     </div>
                     <div class="search__result-book_img">
                     <img src="${imgLibro}" alt="Portada del libro" onerror="this.style.display='none'"> </div>
-                `;
-            seccion.appendChild(article)
-}
+        `;
 
-/* Seccion Recomendado */
-function searchTip(){
-    sectionTips.classList.add("min-height")
-    const title=document.createElement("h2");
-    title.classList.add("section__tip-title","c_Orange")
-    title.textContent="Nuestra sugerencia";
-    const div=document.createElement("div");
-    loadingData(div)
-    sectionTips.append(title,div)
-    let randomNumber = Math.floor(Math.random() * 971); 
-    let url = `https://openlibrary.org/search.json?q=book&limit=1&offset=${randomNumber}`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.docs)
-        return data.docs;})
-    .then(docs=>{
-        div.remove()
-        sectionTips.classList.remove("min-height")
-        createResultsCarts(docs[0],sectionTips)
+    seccion.appendChild(article)
+    const btn = document.querySelector(`.book_inf-${libro.cover_i}`);
+    btn.addEventListener("click", () => {
+        createCartInfoBook(libro)
     })
 }
 
-function loadingData(seccion){
-    seccion.classList.add("loading_data");
-    seccion.innerHTML="Realizando busqueda. Puede tardar unos segundos.."
+/* Seccion Recomendado */
+function searchTip() {
+    sectionTips.classList.add("min-height")
+    const title = document.createElement("h2");
+    title.classList.add("section__tip-title", "c_Orange")
+    title.textContent = "Nuestra sugerencia";
+    const div = document.createElement("div");
+    loadingData(div)
+    sectionTips.append(title, div)
+    let randomNumber = Math.floor(Math.random() * 971);
+    let url = `https://openlibrary.org/search.json?q=book&limit=1&offset=${randomNumber}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.docs)
+            return data.docs;
+        })
+        .then(docs => {
+            div.remove()
+            sectionTips.classList.remove("min-height")
+            createResultsCarts(docs[0], sectionTips)
+        })
 }
 
-createSearch();
-searchTip();
+function loadingData(seccion) {
+    seccion.classList.add("loading_data");
+    seccion.innerHTML = "Realizando busqueda. Puede tardar unos segundos.."
+}
+
+if (sectionSearch) {
+    createSearch();
+    searchTip();
+}
+
+function createCartInfoBook(libro){
+    const nuevaVentana = window.open("libro.html", "_blank");
+    nuevaVentana.onload = function () {
+        const sectionCartBook = nuevaVentana.document.querySelector(".section__cart-book");
+
+        // Crear el contenido
+        const div = nuevaVentana.document.createElement("div");
+        div.classList.add("cart-book__container");
+        div.innerHTML = `
+        <div>
+            <img src="https://covers.openlibrary.org/b/id/${libro.cover_i}-M.jpg" alt="${libro.title}">
+        </div>
+        <div>
+            <h3>${libro.title}</h3>
+            <p><strong>Año de Publicación:</strong> ${libro.first_publish_year || 'N/A'}</p>
+        </div>`;
+
+        // Agregar el contenido a la nueva ventana
+        sectionCartBook.appendChild(div);
+    };
+}
