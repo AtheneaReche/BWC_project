6462ae6c3d1ec2a3a9963a6c3ab9fb5488a4f7ba
Guillermo Pérez <test@gmail.com>
2025-02-18 17:06:51 +0100

    Añadiendo la tarjeta del autor

--- scripts/fiction.js
+++ scripts/fiction.js
@@ -1,4 +1,60 @@
 import { createFooter } from "./footer.js"
 
+const booksContainer = document.getElementById("booksContainer")
+const authorContainer = document.getElementById("authorContainer")
+
+fetch("https://openlibrary.org/authors/OL1394865A.json")
+.then(res => res.json())
+.then(data => imprimirAutor(data))
+
+fetch("https://openlibrary.org/authors/OL1394865A/works.json?limit=10")
+.then(res => res.json())
+.then(data => imprimirLibros(data))
+
+
+
+
+function imprimirAutor(datos){
+    authorContainer.innerHTML = `<img src="../images/brandon.png" alt="Brandon" class="author__img">
+                                <div class="author__info-container">
+                                    <p class="author__info-date text">Fecha de nacimiento: ${datos.birth_date}</p>
+                                    <p class="author__info-bio text">Biografía: ${datos.bio}</p>
+                                </div>`
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const footer=document.querySelector(".footer");
 createFooter("url","url",footer); //-------------------------INCORPORAR LINKS
